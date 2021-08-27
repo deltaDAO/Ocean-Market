03dd2e4d259bb7eae248367f496d7dcc86e7b81f
Moritz Kirstein <mkirstein@protonmail.com>
2021-08-27 16:51:26 +0200

    Update Menu.tsx

--- src/components/molecules/Menu.tsx
+++ src/components/molecules/Menu.tsx
@@ -17,9 +17,7 @@ export default function Menu(): ReactElement {
     <nav className={styles.menu}>
       <Link to="/" className={styles.logo}>
         <Logo branding />
-        <h1 className={styles.title}>
-          {siteTitle}
-        </h1>
+        <h1 className={styles.title}>{siteTitle}</h1>
       </Link>
       <div className={styles.actions}>
         <UserPreferences />
